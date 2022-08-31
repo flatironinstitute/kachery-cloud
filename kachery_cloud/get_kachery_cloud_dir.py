8240dcf14f90302e8265a29793d4e2de445a0882
Jeremy Magland <jmagland@flatironinstitute.org>
2022-08-31 10:37:16 -0400

    move sandbox dir to kachery-cloud dir

--- kachery_cloud/get_kachery_cloud_dir.py
+++ kachery_cloud/get_kachery_cloud_dir.py
@@ -1,8 +1,29 @@
 import os
-import tempfile
 import random
 import atexit
 import shutil
+
+def use_sandbox(use: bool=True):
+    if use:
+        os.environ['KACHERY_CLOUD_USE_SANDBOX'] = '1'
+        if not os.path.exists(os.environ['KACHERY_CLOUD_SANDBOX_DIR']):
+            # only create the dir when we are actually switching to sandbox mode
+            os.makedirs(os.environ['KACHERY_CLOUD_SANDBOX_DIR'])
+            # important to do it this way so we don't clean up the temp dir in a subprocesss
+            _global['sandbox_dir_to_cleanup'] = os.environ['KACHERY_CLOUD_SANDBOX_DIR']
+    else:
+        # We turn off the sandbox, but if we turn it back on again, we want to use the same dir for this process
+        os.environ['KACHERY_CLOUD_USE_SANDBOX'] = '0'
+
+def get_kachery_cloud_dir():
+    from pathlib import Path
+    if os.getenv('KACHERY_CLOUD_USE_SANDBOX', '') == '1':
+        return os.environ['KACHERY_CLOUD_SANDBOX_DIR']
+    homedir = str(Path.home())
+    hsd = os.getenv('KACHERY_CLOUD_DIR', f'{homedir}/.kachery-cloud')
+    if not os.path.exists(hsd):
+        os.makedirs(hsd)
+    return hsd
 
 def _random_string(num_chars: int) -> str:
     chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
@@ -11,7 +32,8 @@ def _random_string(num_chars: int) -> str:
 # We use environ variables for this so that the info is passed on to subprocesses
 if os.getenv('KACHERY_CLOUD_SANDBOX_DIR', None) is None:
     # We don't actually mkdir until it is used
-    os.environ['KACHERY_CLOUD_SANDBOX_DIR'] = f'{tempfile.gettempdir()}/kachery.cloud.sandbox.{_random_string(8)}'
+    # I found that electron remaps the /tmp directory, so we can't use /tmp. Probably just as well.
+    os.environ['KACHERY_CLOUD_SANDBOX_DIR'] = f'{get_kachery_cloud_dir()}/sandbox/{_random_string(8)}'
 if os.getenv('KACHERY_CLOUD_USE_SANDBOX', None) is None:
     os.environ['KACHERY_CLOUD_USE_SANDBOX'] = '0'
 
@@ -26,25 +48,3 @@ def at_exit():
             shutil.rmtree(sandbox_dir_to_cleanup)
 
 atexit.register(at_exit)
-
-def use_sandbox(use: bool=True):
-    if use:
-        os.environ['KACHERY_CLOUD_USE_SANDBOX'] = '1'
-        if not os.path.exists(os.environ['KACHERY_CLOUD_SANDBOX_DIR']):
-            # only create the dir when we are actually switching to sandbox mode
-            os.mkdir(os.environ['KACHERY_CLOUD_SANDBOX_DIR'])
-            # important to do it this way so we don't clean up the temp dir in a subprocesss
-            _global['sandbox_dir_to_cleanup'] = os.environ['KACHERY_CLOUD_SANDBOX_DIR']
-    else:
-        # We turn off the sandbox, but if we turn it back on again, we want to use the same dir for this process
-        os.environ['KACHERY_CLOUD_USE_SANDBOX'] = '0'
-
-def get_kachery_cloud_dir():
-    from pathlib import Path
-    if os.environ['KACHERY_CLOUD_USE_SANDBOX'] == '1':
-        return os.environ['KACHERY_CLOUD_SANDBOX_DIR']
-    homedir = str(Path.home())
-    hsd = os.getenv('KACHERY_CLOUD_DIR', f'{homedir}/.kachery-cloud')
-    if not os.path.exists(hsd):
-        os.makedirs(hsd)
-    return hsd
